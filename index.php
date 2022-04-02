f27348f5e3067410ca1514094377015252f4e6b5
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-04-02 15:49:26 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>List Files</title>
+	<style>
+		body{
+		background-color:lightblue;
+		}
+	</style>
+</head>
+<body>
+	<h1>List Files</h1>
+	<ul>
+		<li><a href="sekolah-programing">sekolah programing</a></li>
+		<li><a href="percobaan">percobaan</a></li>
+	</ul>
+</body>
+</html>
+
+<?php include_once("ip.php"); ?>
+
